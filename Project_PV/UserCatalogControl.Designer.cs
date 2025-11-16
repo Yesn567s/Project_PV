bc462d8e537481aeb2eabdd83f07965551463c66
ynnek <punyakennyh@gmail.com>
2025-11-16 21:34:21 +0700

    Added UI for Catalog and Accessories

--- Project_PV/UserCatalogControl.Designer.cs
+++ Project_PV/UserCatalogControl.Designer.cs
@@ -28,10 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.sortByComboBox = new System.Windows.Forms.ComboBox();
+            this.filterByComboBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(64, 145);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1080, 434);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.sortByComboBox);
+            this.panel1.Controls.Add(this.filterByComboBox);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.searchBox);
+            this.panel1.Location = new System.Drawing.Point(64, 64);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1080, 50);
+            this.panel1.TabIndex = 8;
+            // 
+            // sortByComboBox
+            // 
+            this.sortByComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.sortByComboBox.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.sortByComboBox.FormattingEnabled = true;
+            this.sortByComboBox.Location = new System.Drawing.Point(886, 16);
+            this.sortByComboBox.Name = "sortByComboBox";
+            this.sortByComboBox.Size = new System.Drawing.Size(175, 21);
+            this.sortByComboBox.TabIndex = 6;
+            // 
+            // filterByComboBox
+            // 
+            this.filterByComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.filterByComboBox.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.filterByComboBox.FormattingEnabled = true;
+            this.filterByComboBox.Location = new System.Drawing.Point(687, 16);
+            this.filterByComboBox.Name = "filterByComboBox";
+            this.filterByComboBox.Size = new System.Drawing.Size(175, 21);
+            this.filterByComboBox.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(350, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "🔎";
+            // 
+            // searchBox
+            // 
+            this.searchBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.searchBox.Location = new System.Drawing.Point(13, 15);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(331, 20);
+            this.searchBox.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(59, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Showing ... Items";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(60, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(291, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Browse our complete collection of books";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(57, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 37);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Book Catalog";
+            // 
+            // UserCatalogControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "UserCatalogControl";
+            this.Size = new System.Drawing.Size(1200, 680);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox sortByComboBox;
+        private System.Windows.Forms.ComboBox filterByComboBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
