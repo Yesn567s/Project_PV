231aa8e63cfd0becf8df7d392ef845e906006684
ynnek <punyakennyh@gmail.com>
2026-01-08 00:25:44 +0700

    Laporan Penjualan bulanan Category Added

--- Project_PV/Laporan/LaporanPenjualanBulananCategory.cs
+++ Project_PV/Laporan/LaporanPenjualanBulananCategory.cs
@@ -87,6 +87,22 @@ namespace Project_PV.Laporan {
                 return this.ReportDefinition.Sections[4];
             }
         }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.Shared.IParameterField Parameter_Month {
+            get {
+                return this.DataDefinition.ParameterFields[0];
+            }
+        }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.Shared.IParameterField Parameter_GeneratedDate {
+            get {
+                return this.DataDefinition.ParameterFields[1];
+            }
+        }
     }
     
     [System.Drawing.ToolboxBitmapAttribute(typeof(CrystalDecisions.Shared.ExportOptions), "report.bmp")]
